54d39605018165fd191d18d3fc6449a7ce1d18c9
danizaghian <dani.zaghian@gmail.com>
2019-05-01 11:21:32 -0700

    adds looping file

---
+++ loops.rb
@@ -0,0 +1,37 @@
+# cities = ["Jackson", "Southhaven", "Oxford", "Vicksburg", "Booneville"]
+
+# i = 0
+
+# cities.length.times do
+# 	puts "#{cities[i]}, Mississippi"
+# 	i = i + 1
+# end
+
+
+# cities.each do |city|
+# 	puts "#{city}, Mississippi"
+# end
+
+
+number = 0 
+
+# while runs if condition is true
+while number < 4
+	puts "hello world"
+	number += 1
+end
+
+# until runs if condition is false
+until number < 4
+	#do something
+end
+
+# if runs if condition is true
+if number < 4
+	#do something
+end
+
+# unless runs if condition is false
+unless number < 4
+	#do something
+end
